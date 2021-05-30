62c3fca59e25936a005409d00ef5254f6d0fe2d6
linozamora <lino.zamora@uao.edu.co>
2021-05-30 15:57:35 -0500

    menu_principal

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+import Mundo from './mundo.js';
+import Mundo2 from './mundo2.js';
 import Mundo3 from './mundo3.js';
-import Mundo from './mundo3.js';
+import main_menu from './main_menu.js';
+
 const configuracion = {
 
     title: 'proyecto',
@@ -20,6 +23,6 @@ const configuracion = {
     },
 
 
-scene: [Mundo3]
+scene: [main_menu, Mundo, Mundo2 ,Mundo3]
 }
 const game = new Phaser.Game(configuracion);
